5e2d03f08ef4dd5754752b70f8a1a0d30ab16cb6
Ansgar Esztermann <aeszter@gwdg.de>
2017-02-17 16:25:56 +0100

    Error_Reporter: add Global contracts, State

--- src/error_reporter.adb
+++ src/error_reporter.adb
@@ -1,6 +1,6 @@
-with SPARK.Text_IO;
-
-package body Error_Reporter with SPARK_Mode is
+package body Error_Reporter with SPARK_Mode,
+  Refined_State => (State => My_Error)
+is
 
    package IO renames SPARK.Text_IO;
 
@@ -9,9 +9,9 @@ package body Error_Reporter with SPARK_Mode is
       My_Error := False;
    end Clear_Error;
 
-   procedure Error (Line : Positive; Message : String) is
+   procedure Error (Line_No : Positive; Message : String) is
    begin
-      Report (Line    => Line,
+      Report (Line_No => Line_No,
               Where   => "",
               Message => Message);
    end Error;
@@ -21,10 +21,15 @@ package body Error_Reporter with SPARK_Mode is
       return My_Error;
    end Had_Error;
 
-   procedure Report (Line : Positive; Where, Message : String) is
+   procedure Report (Line_No : Positive; Where, Message : String) is
    begin
-      IO.Put_Line (IO.Standard_Error,
-                   "[line" & Line'Img & "] Error" & Where & ": " & Message);
+      if Status (Standard_Error) = Success and then Is_Writable (Standard_Error) then
+         IO.Put_Line (Standard_Error,
+                      "[line" & Integer'Image (Line_No) & "] Error" & Where & ": " & Message);
+      end if;
+      -- why is this here rather than in procedure Error?
+      -- it is not part of the report, after all, and we could put the Status/Writable
+      -- check inside Error, around the call to Report
       My_Error := True;
    end Report;
 
--- src/error_reporter.ads
+++ src/error_reporter.ads
@@ -1,8 +1,15 @@
-package Error_Reporter with SPARK_Mode is
-   procedure Error (Line : Positive; Message : String);
-   procedure Report (Line : Positive; Where, Message : String);
+with SPARK.Text_IO; use SPARK.Text_IO;
+
+package Error_Reporter with SPARK_Mode,
+  abstract_state => State,
+    Initializes => State
+is
+   procedure Error (Line_No : Positive; Message : String) with
+     Global => (in_out => Standard_Error, Output => State);
+   procedure Report (Line_No : Positive; Where, Message : String) with
+     Global => (in_out => Standard_Error, Output => State);
    function Had_Error return Boolean;
    procedure Clear_Error;
 private
-   My_Error : Boolean := False;
+   My_Error : Boolean := False with Part_Of => State;
 end Error_Reporter;
