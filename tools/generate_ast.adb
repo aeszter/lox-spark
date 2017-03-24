f7f1b074337dea3555750eaa9be0a2830e4ccd02
Ansgar Esztermann <aeszter@gwdg.de>
2017-03-24 09:52:10 +0100

    Generate_Ast: Generic units cannot be overloaded
    -change name Visit (original: Accept, but that is an Ada Keyword) to Accept_Visitor for clarity;
    -make Accept_Visitor non-generic, and therefore a procedure;
    -add Define_Full_Type and Define_Subprogram subprograms;
    -base Expr type must be abstract;
    -proper casing of generated source file names;
    -various layout fixes.

--- tools/generate_ast.adb
+++ tools/generate_ast.adb
@@ -12,12 +12,18 @@ procedure Generate_Ast is
    procedure Define_Ast (Output_Dir, Base_Name : String; Types : String_Array);
    procedure Define_Type (Spec_File, Body_File : IO.File_Type;
                           Base_Name, Class_Name, Field_List : String);
+   procedure Define_Full_Type (Spec_File, Body_File : IO.File_Type;
+                          Base_Name, Class_Name, Field_List : String);
+   procedure Define_Subprogram (Spec_File, Body_File : IO.File_Type;
+                          Base_Name, Class_Name, Field_List : String);
    procedure Define_Visitor (File : IO.File_Type; Base_Name : String; Types : String_Array);
    function Substring (Input, Separator : String; Item_Number : Positive) return String;
 
    procedure Define_Ast (Output_Dir, Base_Name : String; Types : String_Array) is
-      Spec_Path : constant String := Output_Dir & "/" & Base_Name & ".ads";
-      Body_Path : constant String := Output_Dir & "/" & Base_Name & ".adb";
+      Spec_Path : constant String := Output_Dir & "/"
+                    & Ada.Characters.Handling.To_Lower (Base_Name) & "s.ads";
+      Body_Path : constant String := Output_Dir & "/"
+                    & Ada.Characters.Handling.To_Lower (Base_Name) & "s.adb";
       Spec_File, Body_File : IO.File_Type;
    begin
       IO.Create (File => Spec_File, Name => Spec_Path);
@@ -25,8 +31,7 @@ procedure Generate_Ast is
 
       IO.Put_Line (Spec_File, "package " & Base_Name & "s is");
       IO.Put_Line (Body_File, "package body " & Base_Name & "s is");
-      IO.Put_Line (Spec_File, "   type " & Base_Name & " is tagged private;");
-      Define_Visitor (Spec_File, Base_Name, Types);
+      IO.Put_Line (Spec_File, "   type " & Base_Name & " is abstract tagged null record;");
       -- The AST classes.
       for The_Type of Types loop
          declare
@@ -36,13 +41,31 @@ procedure Generate_Ast is
             Define_Type (Spec_File, Body_File, Base_Name, Class_Name, Fields);
          end;
       end loop;
-
+      Define_Visitor (Spec_File, Base_Name, Types);
+      for The_Type of Types loop
+         declare
+            Class_Name : constant String := Substring (The_Type.all, ":", 1);
+            Fields     : constant String := Substring (The_Type.all, ":", 2);
+         begin
+            Define_Subprogram (Spec_File, Body_File, Base_Name, Class_Name, Fields);
+         end;
+      end loop;
+      IO.New_Line (Spec_File);
       -- The base accept() method.
-      IO.Put_Line (Spec_File, "");
       IO.Put_Line (Spec_File, "--  abstract <R> R accept(Visitor<R> visitor);");
-      IO.Put_Line (Spec_File, "  function Visit_Expr (Self : Expr; V : Visitor) return R is abstract;");
+      IO.Put_Line (Spec_File, "   procedure Accept_Visitor (Self : Expr; V : Visitors.Visitor) is abstract;");
 
-      IO.Put_Line (Spec_File, "end;");
+      IO.Put_Line (Spec_File, "private");
+      for The_Type of Types loop
+         declare
+            Class_Name : constant String := Substring (The_Type.all, ":", 1);
+            Fields     : constant String := Substring (The_Type.all, ":", 2);
+         begin
+            Define_Full_Type (Spec_File, Body_File, Base_Name, Class_Name, Fields);
+         end;
+      end loop;
+
+      IO.Put_Line (Spec_File, "end " & Base_Name & "s;");
       IO.Close (Spec_File);
       pragma Unreferenced (Spec_File);
       IO.Put_Line (Body_File, "end;");
@@ -50,77 +73,78 @@ procedure Generate_Ast is
       pragma Unreferenced (Body_File);
    end Define_Ast;
 
-   procedure Define_Type (Spec_File, Body_File : IO.File_Type;
+   procedure Define_Full_Type (Spec_File, Body_File : IO.File_Type;
                           Base_Name, Class_Name, Field_List : String) is
+      pragma Unreferenced (Body_File);
       use Ada.Strings.Fixed;
 
       Before, After : Natural := Field_List'First;
    begin
-      IO.Put_Line (Spec_File, "");
-      IO.Put_Line (Spec_File, "--  static class " & Class_Name & " extends " &
-                     Base_Name & " {");
-      IO.Put_Line (Spec_File, "type " & Class_Name & " is new " & Base_Name & ";");
-      IO.Put_Line (Spec_File, "function Visit (Self : " & Class_Name &
-        "; The_Visitor : Visitor) return R;");
+      IO.Put (Spec_File, "   type " & Class_Name & " is new " & Base_Name & " with ");
 
-      -- Constructor.
-      IO.Put_Line (Spec_File, "--    " & Class_Name & "(" & Field_List & ") {");
+      if Field_List'Length > 0 then
+         IO.Put_Line (Spec_File, "record");
+         while After < Field_List'Last loop
+            Before := After;
+            After := Index (Source  => Field_List,
+                            Pattern => ", ",
+                            From    => Before);
+            declare
+               Field : constant String := Field_List (Before + 1 .. After - 1);
+               Space : constant Natural := Index (Source => Field,
+                                        Pattern => " ",
+                                        From    => Field'First);
+               Name  : constant String := Field (Field'First .. Space - 1);
+            begin
+               IO.Put_Line (Spec_File, "      this." & Name & " = " & Name & ";");
+            end;
+         end loop;
+         IO.Put_Line (Spec_File, "end record;");
+      else
+         IO.Put_Line (Spec_File, "null record;");
+      end if;
+   end Define_Full_Type;
 
-      while After < Field_List'Last loop
-         Before := After;
-         After := Index (Source  => Field_List,
-                         Pattern => ", ",
-                         From    => Before);
-         declare
-            Field : constant String := Field_List (Before + 1 .. After - 1);
-            Space : constant Natural := Index (Source => Field,
-                                     Pattern => " ",
-                                     From    => Field'First);
-            Name  : constant String := Field (Field'First .. Space - 1);
-         begin
-            IO.Put_Line (Spec_File, "      this." & Name & " = " & Name & ";");
-         end;
-      end loop;
+   procedure Define_Subprogram (Spec_File, Body_File : IO.File_Type;
+                          Base_Name, Class_Name, Field_List : String) is
+      pragma Unreferenced (Field_List);
+      use Ada.Strings.Fixed;
 
-      IO.Put_Line (Spec_File, "--    }");
+   begin
       -- Visitor pattern.
       IO.New_Line (Spec_File);
       IO.Put_Line (Spec_File, "--    <R> R accept(Visitor<R> visitor) {");
       IO.Put_Line (Spec_File, "--      return visitor.visit" &
         Class_Name & Base_Name & "(this);");
       IO.Put_Line (Spec_File, "--    }");
-      IO.Put_Line (Spec_File, " generic");
-      IO.Put_Line (Spec_File, "   type R;");
-      IO.Put_Line (Spec_File, " function Visit (Self : " & Class_Name & "; V : Visitor) return R;");
-      IO.Put_Line (Body_File, " generic");
-      IO.Put_Line (Body_File, "   type R;");
-      IO.Put_Line (Body_File, " function Visit (Self : " & Class_Name & "; V : Visitor) return R is");
+      IO.Put_Line (Spec_File, "   procedure Accept_Visitor (Self : " & Class_Name & "; V : Visitors.Visitor);");
+      IO.Put_Line (Body_File, " procedure Accept_Visitor (Self : " & Class_Name & "; V : Visitor) is");
       IO.Put_Line (Body_File, " begin");
-      IO.Put_Line (Body_File, "    return V.Visit" & Class_Name & Base_Name & " (Self);");
-      IO.Put_Line (Body_File, " end Visit;");
+      IO.Put_Line (Body_File, "    V.Visit" & Class_Name & Base_Name & " (Self);");
+      IO.Put_Line (Body_File, " end Accept_Visitor;");
 
       -- Fields.
       IO.New_Line (Spec_File);
-      After := Field_List'First;
-      while After < Field_List'Last loop
-         Before := After;
-         After := Index (Source  => Field_List,
-                         Pattern => ", ",
-                         From    => Before);
-         declare
-            Field : constant String := Field_List (Before + 1 .. After - 1);
-         begin
-            IO.Put_Line (Spec_File, "    final " & Field & ";");
-         end;
-      end loop;
 
       IO.Put_Line (Spec_File, "--  }");
+   end Define_Subprogram;
+
+   procedure Define_Type (Spec_File, Body_File : IO.File_Type;
+                          Base_Name, Class_Name, Field_List : String) is
+      pragma Unreferenced (Body_File, Field_List);
+      use Ada.Strings.Fixed;
+
+   begin
+      IO.Put_Line (Spec_File, "");
+      IO.Put_Line (Spec_File, "--  static class " & Class_Name & " extends " &
+                     Base_Name & " {");
+      IO.Put_Line (Spec_File, "   type " & Class_Name & " is new " & Base_Name & " with private;");
+
+      IO.Put_Line (Spec_File, "--    }");
    end Define_Type;
 
    procedure Define_Visitor (File : IO.File_Type; Base_Name : String; Types : String_Array) is
    begin
-      IO.Put_Line (File, "   generic");
-      IO.Put_Line (File, "      type R;");
       IO.Put_Line (File, "   package Visitors is");
       IO.Put_Line (File, "      type Visitor is interface;");
       IO.Put_Line (File, "--  interface Visitor<R> {");
@@ -129,15 +153,15 @@ procedure Generate_Ast is
          declare
             Type_Name : constant String  := Substring (The_Type.all, ":", 1);
          begin
-            IO.Put_Line (File, "   function Visit_" & Type_Name & "_" & Base_Name &
+            IO.Put_Line (File, "      procedure Visit_" & Type_Name & "_" & Base_Name &
                            " (Self : Visitor; The_" & Base_Name &
-                           " : " & Type_Name & ") return R is abstract;");
+                           " : " & Type_Name & ") is abstract;");
             IO.Put_Line (File, "--    R visit" & Type_Name & Base_Name & "(" &
                   Type_Name & " " & Ada.Characters.Handling.To_Lower (Base_Name) & ");");
          end;
       end loop;
 
-      IO.Put_Line (File, "end Visitors;");
+      IO.Put_Line (File, "   end Visitors;");
    end Define_Visitor;
 
    function Substring (Input, Separator : String; Item_Number : Positive) return String is
