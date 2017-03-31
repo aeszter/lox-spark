70e53ef9393e983fed1442a3665dcaf40c6a27c0
Ansgar Esztermann <aeszter@gwdg.de>
2017-03-31 15:10:08 +0200

    Add test_ast
    -create a simple tree;
    -use Ast_Printers to output a textual representation.

---
+++ tools/test_ast.adb
@@ -0,0 +1,15 @@
+with Ada.Text_IO;
+with Ast_Printers;
+with Exprs; use Exprs;
+with Storage;
+with Tokens; use Tokens;
+
+procedure Test_Ast is
+   Test_Expr : Expr_Handle;
+begin
+   Test_Expr := Create_Binary (Create_Unary (New_Token (T_MINUS, "-", 1),
+                                 Create_Num_Literal (123)),
+                               New_Token (T_STAR, "*", 1),
+                               Create_Grouping (Create_Float_Literal (45.67)));
+   Ada.Text_IO.Put_Line (Ast_Printers.Print (Storage.Retrieve (Test_Expr)));
+end Test_Ast;
